a6dde92b5e7e4842de4ba3001f1c8cffd7af6791
Jose Carlos Serufo Filho <serufo.ufmg@gmail.com>
2018-09-25 15:30:07 -0300

    Inclusão do botao e condigo de mudança da activity

--- app/src/main/java/com/example/ads_2018_2/MainActivity.java
+++ app/src/main/java/com/example/ads_2018_2/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.ads_2018_2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Codigo para mudar para pagina do SERUFO
+
+        Button btnSerufo = findViewById(R.id.btnSerufo);
+        btnSerufo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Serufo.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
