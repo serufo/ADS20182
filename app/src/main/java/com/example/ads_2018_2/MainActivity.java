8d51ee7c84e8c36c73c5f15bd140df1e60920b8a
Serufo <serufo.ufmg@gmail.com>
2018-09-25 17:26:09 -0300

    Criamos um botao para a nova activity

--- app/src/main/java/com/example/ads_2018_2/MainActivity.java
+++ app/src/main/java/com/example/ads_2018_2/MainActivity.java
@@ -28,6 +28,19 @@ public class MainActivity extends AppCompatActivity {
 
         //[SERUFO] Fim do código do SERUFO
 
+        //[SERUFO2] Codigo para mudar para pagina do SERUFO2
+
+        Button btnSerufo2 = findViewById(R.id.btnSerufo2);
+        btnSerufo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Serufo2.class);
+                startActivity(intent);
+            }
+        });
+
+        //[SERUFO2] Fim do código do SERUFO2
+
 
         //[Marcos] Meu código
         Button btnMarcos = findViewById(R.id.btnMarcos);
