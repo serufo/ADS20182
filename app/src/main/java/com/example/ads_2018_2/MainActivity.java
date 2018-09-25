0a25af9e0dbb88c314b8ba9d2eb5f54d4e0a228f
marcus-oliveiraads <mv_lainea7x@hotmail.com>
2018-09-25 17:26:46 -0300

    Criamos um botão para a nova activity

--- app/src/main/java/com/example/ads_2018_2/MainActivity.java
+++ app/src/main/java/com/example/ads_2018_2/MainActivity.java
@@ -28,6 +28,19 @@ public class MainActivity extends AppCompatActivity {
 
         //[SERUFO] Fim do código do SERUFO
 
+        //[MARCUS] Codigo para mudar para pagina do MARCUS
+
+        Button btnMarcus = findViewById(R.id.btnMarcus);
+        btnSerufo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Marcus.class);
+                startActivity(intent);
+            }
+        });
+
+        //[MARCUS] Fim do código do MARCUS
+
 
     }
 }
