09eb539d69e3248f9ba17347a880901c44ceff72
Jose Carlos Serufo Filho <serufo.ufmg@gmail.com>
2018-09-25 15:53:10 -0300

    Botao Voltar na activity Serufo

--- app/src/main/java/com/example/ads_2018_2/Serufo.java
+++ app/src/main/java/com/example/ads_2018_2/Serufo.java
@@ -3,6 +3,7 @@ package com.example.ads_2018_2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class Serufo extends AppCompatActivity {
@@ -14,7 +15,13 @@ public class Serufo extends AppCompatActivity {
 
         //Botao voltar
         Button btnVoltar = findViewById(R.id.btnVoltar);
-        Intent intent = new Intent(Serufo.this, MainActivity.class);
-        startActivity(intent);
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Serufo.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
